2aa985d9b8602e45bfa16945c777c7deb3ebea41
riham <riham91.alsiyabi@gmail.com>
2025-03-23 12:25:15 +0400

    complet part 1

--- Program.cs
+++ Program.cs
@@ -12,45 +12,85 @@ namespace SimpleStudentManagementProject
         static int count = 0;
         static void Main(string[] args)
         {
+            {
+                while (true) // we use while loop to repeat the process and we set true so it will not stop ... 
+                {
+                    Console.Clear();
+                    Console.WriteLine("System Menu \n");
+                    Console.WriteLine("Select option: ");
+                    Console.WriteLine("1. Adding a New Student");
+                    Console.WriteLine("2. Viewing All Students");
+                    Console.WriteLine("3. Find Student By Name");
+                    Console.WriteLine("4. Calculating the Class Average");
+                    Console.WriteLine("5. Find TheTop Pe rForming Student()");
+                    Console.WriteLine("6. Sort Students By Marks()");
+                    Console.WriteLine("7. Delete A Student Record()");
+                    Console.WriteLine("0.  Exit");
+
+                    Console.Write("\nEnter your option: \n");
+                    int choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1: addNewStudentRecord(); break;
+                        case 2: ViewAllStudents(); break;
+                        case 3: FindStudentByName(); break;
+                        case 4: CalculateTheClassAverage(); break;
+                        case 5: FindTheTopPerFormingStudent(); break;
+                        case 6: SortStudentsByMarks(); break;
+                        case 7: DeleteAStudentRecord(); break;
+                        case 0: Console.WriteLine("Have a nice day ..."); return;
+                        default: Console.WriteLine("\n You enter unaccepted option! ... try again"); break;
+                    }
+                    Console.ReadLine();// we add this line just to stop the program from clear 'Console.Clear();' the screen before the user see the result ...
+
+                }
+            }
+
+
+
+
+
+
             //1.Add a new student record(Name, Age, Marks)
             static void addNewStudentRecord()
             {
 
-                for (int i = 0; i < studentName.Length; i++)
+                char ChoiceChar = 'y';
+                while (count < 10)
                 {
-                    Console.WriteLine("Enter the Student name :" + count + 1);
-                    studentName[i] = Console.ReadLine();
+                    Console.WriteLine($"Enter the name of student {count + 1}:");
+                    studentName[count] = Console.ReadLine();
+
+                    int Mark;
+                    do
+                    {
+                        Console.WriteLine($"Enter the Mark of student {count + 1} (0-100): ");
+                    }
+                    while (!int.TryParse(Console.ReadLine(), out Mark) || Mark < 0 || Mark > 100);
+                    marks[count] = Mark;
+
+                    int Age;
+                    do
+                    {
+                        Console.WriteLine($"Enter the age of student {count + 1}: (>21): ");
+
+                    } while (!int.TryParse(Console.ReadLine(), out Age) || Age <= 21);
+                    studentAge[count] = Age;
+
+                    dateTimes[count] = DateTime.Now;
+                    Console.WriteLine("Student Add Successfully");
                     count++;
 
-                    Console.WriteLine("Enter the student age");
-                    studentAge[i] = int.Parse(Console.ReadLine());
-
-                    if (studentAge[i] < 21)
-                    {
-                        Console.WriteLine("The age must be greater than 19");
+                    Console.WriteLine("Do you want add another student information ? y / n");
+                    ChoiceChar = Console.ReadKey().KeyChar;
+                    Console.WriteLine();
+                    if (ChoiceChar != 'y' && ChoiceChar != 'Y')
                         break;
-                    }
-
-                    Console.WriteLine("Enter the student mark");
-                    marks[i] = double.Parse(Console.ReadLine());
-                    if (marks[i] < 0 || marks[i] > 100)
-                    {
-                        Console.WriteLine("The marks must be between 0 and 100");
-                        break;
-                    }
-
-                    dateTimes[i] = DateTime.Now;
-                    count++;
-
-                    if (i > count)
-                    {
-                        break;
-
-                    }
-
-                    Console.WriteLine("Enter another student");
-
                 }
+                if (count == 11)
+                    Console.WriteLine("Cannot add more students. Maximum limit reached.");
+
+
             }
 
             //2. ViewAllStudents
@@ -73,7 +113,7 @@ namespace SimpleStudentManagementProject
 
             //3. Find a student by name
             static void FindStudentByName()
-                {
+            {
                 Console.WriteLine("Enter the student name to search");
                 string searchName = Console.ReadLine();
 
@@ -107,14 +147,14 @@ namespace SimpleStudentManagementProject
                 double AverageMarks = sum / count;
                 Console.WriteLine($"Average Marks: {Math.Round(AverageMarks, 2)}");
             }
-            
-       
+
+
             //5.. Find the top-performing student 
             static void FindTheTopPerFormingStudent()
             {
-                double TopMark= marks[0];
+                double TopMark = marks[0];
                 string SName = studentName[0];
-                for (int i = 0;i < studentAge.Length;i++)
+                for (int i = 1; i < studentAge.Length; i++)
                 {
                     if (marks[i] > TopMark)
 
@@ -123,9 +163,9 @@ namespace SimpleStudentManagementProject
                         SName = studentName[i];
 
                     }
-                    Console.WriteLine("student " + SName+  "Top Marks :" + TopMark);
+                   
                 }
-
+                Console.WriteLine("student " + SName + "Top Marks :" + TopMark);
 
 
             }
@@ -133,8 +173,38 @@ namespace SimpleStudentManagementProject
             //6. Sort students by marks
             static void SortStudentsByMarks()
             {
+                Array.Sort(marks, studentName);
+                for (int i = 0; i < studentAge.Length; i++)
+                {
+                    Console.WriteLine("student record" + i + 1);
+                    Console.WriteLine("student name" + studentName[i]);
+                    Console.WriteLine("student age" + studentAge[i]);
+                    Console.WriteLine("student marks" + marks[i]);
+                    Console.WriteLine("date and time" + dateTimes[i]);
+                }
+                Console.ReadLine();
+
 
             }
-        } 
-    }       
+
+            //7. Delete a student record 
+            static void DeleteAStudentRecord()
+            {
+                Console.WriteLine("Enter the student name to delete");
+                string deleteName = Console.ReadLine();
+                for (int i = 0; i < studentName.Length; i++)
+                {
+                    if (studentName[i] == deleteName)
+                    {
+                        studentName[i] = null;
+                        studentAge[i] = 0;
+                        marks[i] = 0;
+                        dateTimes[i] = DateTime.MinValue;
+                    }
+                }
+
+            }
+
+        }
+    }
 }
